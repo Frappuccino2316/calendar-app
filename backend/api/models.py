055abb0d63267595be541fc887a329760eb458e2
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-06 19:05:48 +0900

    記述順序を変更

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.team_name
+
+
 class Task(models.Model):
     title = models.CharField(max_length=50)
     team_in_charge = models.ForeignKey(Team, on_delete=models.CASCADE)
@@ -9,10 +18,3 @@ class Task(models.Model):
     def __str__(self):
         return self.title
     
-class Team(models.Model):
-    team_name = models.CharField(max_length=50)
-    create_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.team_name
