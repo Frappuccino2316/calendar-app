9c8aad1d531b03a7d0d4dcbed6ab5087116c6f95
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-06 19:00:55 +0900

    TaskモデルとTeamモデルを作成

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(model.Model):
+    title = models.CharField(max_length=50)
+    team_in_charge = models.ForeinField(Team, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Team(model.Model):
+    team_name = models.CharField(max_length=50)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
