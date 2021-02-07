b0251239c120bbafd962feb56dc4fe0eef77115f
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-07 23:34:18 +0900

    viewを作成

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Task, Team
+from rest_framework import viewsets, generics
+from .serializers import UserSerializer, TaskSerializer, TeamSerializer
+from .ownpermissions import ProfilePermission
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (ProfilePermission,)
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    
+    def get_object(self):
+        return self.request.user
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    
