5ea2ae3bb1feed5a7259b56daa27c00b086544dc
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-08 23:40:47 +0900

    Create, Update, Deleteのみ許可した

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework import generics
+from rest_framework import generics, mixins
 from users.models import Users
 from .models import Task, Team
 from rest_framework import viewsets, generics
 from .serializers.serializers import UserSerializer, TaskSerializer, TeamSerializer
 from .ownpermissions import ProfilePermission
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
     permission_classes = (ProfilePermission,)
+    
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
