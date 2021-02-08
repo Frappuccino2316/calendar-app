fbaccc7c6d3c10b3a7bbba63817f3dc7c2949389
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-08 23:40:28 +0900

    POSTは誰でも許可し、それ以外はidとログインユーザーが一致した場合許可

--- backend/api/ownpermissions.py
+++ backend/api/ownpermissions.py
@@ -2,6 +2,8 @@ from rest_framework import permissions
 
 class ProfilePermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method == 'POST':
+            return True
+        if obj.id == request.user.id:
             return True
         return False
