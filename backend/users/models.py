c570734d3b6c49777f0eb2728857997c3af2799c
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-09 23:16:27 +0900

    emailを必須項目に設定

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from api.models import Team
 
 class Users(AbstractUser):
+    email = models.EmailField(_('email address'), null=False)
     team_of_affiliation = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
     
