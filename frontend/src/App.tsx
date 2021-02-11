22c93514a0a2b8ed565645e4232da57f2e2b9eee
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-11 23:57:57 +0900

    App.tsxを整形

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import axios from "axios";
-import "./App.css";
+import React from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
-  const [teams, setTeams] = React.useState([]);
+  const [teams, setTeams] = React.useState([])
 
   React.useEffect(() => {
     // const res = async () => {
@@ -19,18 +19,18 @@ function App() {
     // };
     // res();
     axios
-      .get("http://localhost:8000/api/v1/teams/", {
+      .get('http://localhost:8000/api/v1/teams/', {
         headers: {
           Authorization:
-            "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEzMDUzNTA1LCJqdGkiOiJmMWEyMWU4ZDAwODQ0YmQ4OWQxYjJiYTVmZGZiZDJlMSIsInVzZXJfaWQiOjJ9.nRkgM1JLg5jpxmpgEy3VNZgNQeOU694ULFoaRyUu7b0",
+            'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEzMDUzNTA1LCJqdGkiOiJmMWEyMWU4ZDAwODQ0YmQ4OWQxYjJiYTVmZGZiZDJlMSIsInVzZXJfaWQiOjJ9.nRkgM1JLg5jpxmpgEy3VNZgNQeOU694ULFoaRyUu7b0',
         },
       })
       .then((res) => setTeams(res.data))
-      .catch((e) => console.log("error"));
-  }, []);
-  console.log(teams);
+      .catch((e) => console.log('error'))
+  }, [])
+  console.log(teams)
 
-  return <div className="App">a</div>;
+  return <div className="App">a</div>
 }
 
-export default App;
+export default App
