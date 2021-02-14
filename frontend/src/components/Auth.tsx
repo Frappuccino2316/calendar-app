adfb35fd9abcb7203982bb49d1688d278f16cea4
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-15 00:51:49 +0900

    tokenのcalendarJWTを確認するようにした

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,23 +1,23 @@
-import React, { ReactNode } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
-import verifyToken from '../commons/auth/verifyToken'
+import React, { ReactNode } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import verifyToken from 'commons/auth/verifyToken';
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 const Auth: React.FC<Props> = ({ children }) => {
-  const [cookie] = useCookies()
-  const history = useHistory()
+  const [cookie] = useCookies();
+  const history = useHistory();
   React.useEffect(() => {
-    if (cookie.tes === undefined) {
-      history.push('/login')
+    if (cookie.calendarJWT === undefined) {
+      history.push('/login');
     }
-    const isAuthenticated = verifyToken(cookie.calendarJWT)
-    isAuthenticated && history.push('/login')
-  })
-  return <div>{children}</div>
-}
+    const isAuthenticated = verifyToken(cookie.calendarJWT);
+    !isAuthenticated && history.push('/login');
+  });
+  return <div>{children}</div>;
+};
 
-export default Auth
+export default Auth;
