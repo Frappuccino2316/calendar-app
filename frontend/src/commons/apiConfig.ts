f57ae9180d86f1e95681165921d8c45965e46306
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-15 00:40:38 +0900

    細かい修正

--- frontend/src/commons/apiConfig.ts
+++ frontend/src/commons/apiConfig.ts
@@ -1,6 +1,6 @@
-type Url = {
-  apiKey: string;
-  apiBaseUrl: string;
+type ApiConfig = {
+  apiKey: string | undefined;
+  apiUrl: string | undefined;
 };
 
 const Config = () => {
@@ -8,22 +8,22 @@ const Config = () => {
     return {
       restful: {
         apiKey: process.env.REACT_APP_TEST_API_KEY,
-        apiURL: process.env.REACT_APP_TEST_BASE_URL,
+        apiUrl: process.env.REACT_APP_TEST_BASE_URL,
       },
     };
   } else if (process.env.NODE_ENV === 'production') {
     return {
       restful: {
         apiKey: process.env.REACT_APP_PRODUCTION_API_KEY,
-        apiURL: process.env.REACT_APP_PRODUCTION_BASE_URL,
+        apiUrl: process.env.REACT_APP_PRODUCTION_BASE_URL,
       },
     };
   }
   return {
     restful: {
       apiKey: process.env.REACT_APP_DEVELOPMENT_API_KEY,
-      apiURL: process.env.REACT_APP_DEVELOPMENT_BASE_URL,
+      apiUrl: process.env.REACT_APP_DEVELOPMENT_BASE_URL,
     },
   };
 };
-export const apiConfig = Config().restful;
+export const apiConfig: ApiConfig = Config().restful;
