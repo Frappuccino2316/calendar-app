a0ac4651265f6c92cb1dc86bed60a47775b49d3b
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-15 23:40:47 +0900

    Logoutコンポーネントを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Auth from './components/Auth';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Logout from './components/Logout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Logout />
         <Route exact path="/login" component={Login} />
         <Auth>
           <Route exact path="/" component={Home} />
