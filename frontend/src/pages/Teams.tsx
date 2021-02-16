9e39c931f464b07b973ed34c4df9ff2a89ae3019
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-16 23:56:44 +0900

    ページを作成

---
+++ frontend/src/pages/Teams.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Teams: React.FC = () => {
+  // const [team, setTeam] = React.useState('');
+
+  // React.useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <h1>Teams</h1>
+      <p>所属チーム</p>
+      {/* <p>{teamName}</p> */}
+    </div>
+  );
+};
+
+export default Teams;
