31d68b1f6936b851df50ae10b5b04ec8e061ffe3
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-16 22:31:02 +0900

    Appコンポーネントを修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Auth from './components/Auth';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Logout from './components/Logout';
+import Auth from 'components/Auth';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Logout from 'components/Logout';
+import Header from 'components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Logout />
         <Route exact path="/login" component={Login} />
         <Auth>
