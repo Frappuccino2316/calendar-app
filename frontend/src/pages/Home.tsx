0ec5ca77dda8256fe10595844f658a799e9acd8f
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-16 23:01:07 +0900

    material-uiでリンクを追加

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted'
-// import GroupIcon from '@material-ui/icons/Group'
-// import SettingsIcon from '@material-ui/icons/Settings'
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import GroupIcon from '@material-ui/icons/Group';
+import SettingsIcon from '@material-ui/icons/Settings';
 import 'pages/Home.css';
 import Title from 'components/Title';
 
@@ -36,7 +36,7 @@ const Home: React.FC = () => {
           <div className="block link_tasks">
             Task
             <span className="icon">
-              {/* <FormatListBulletedIcon style={styles.largeStyle} /> */}
+              <FormatListBulletedIcon style={styles.largeStyle} />
             </span>
           </div>
         </Link>
@@ -45,7 +45,7 @@ const Home: React.FC = () => {
             <div className="block link_teams">
               Team
               <span className="icon">
-                {/* <GroupIcon style={styles.normalStyle} /> */}
+                <GroupIcon style={styles.normalStyle} />
               </span>
             </div>
           </Link>
@@ -53,7 +53,7 @@ const Home: React.FC = () => {
             <div className="block link_settings">
               Setting
               <span className="icon">
-                {/* <SettingsIcon style={styles.normalStyle} /> */}
+                <SettingsIcon style={styles.normalStyle} />
               </span>
             </div>
           </Link>
