aa87ed280386d681d8edb4257a48bad617334481
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-16 23:56:38 +0900

    各ページを作成

---
+++ frontend/src/pages/Settings.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Settings: React.FC = () => {
+  // React.useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <p>Settings</p>
+      <span>ユーザー名</span>
+      <span>メールアドレス</span>
+    </div>
+  );
+};
+
+export default Settings;
