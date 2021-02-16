3428f90b0701b2246e3b70816a791359f11804c5
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-16 23:01:23 +0900

    Logoutを削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Auth from 'components/Auth';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
-import Logout from 'components/Logout';
 import Header from 'components/Header';
 import './App.css';
 
@@ -12,7 +11,6 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Logout />
         <Route exact path="/login" component={Login} />
         <Auth>
           <Route exact path="/" component={Home} />
