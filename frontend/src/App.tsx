226d28d1e5adf7e28cf2affc4c2a3887ad8b1504
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-18 12:59:24 +0900

    AppでAuthコンポーネントで囲うのをやめた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from 'components/Auth';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Tasks from 'pages/Tasks';
@@ -16,12 +15,10 @@ function App() {
         <Header />
         {/* <Switch> */}
         <Route exact path="/login" component={Login} />
-        <Auth>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/tasks" component={Tasks} />
-          <Route exact path="/teams" component={Teams} />
-          <Route exact path="/settings" component={Settings} />
-        </Auth>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/tasks" component={Tasks} />
+        <Route exact path="/teams" component={Teams} />
+        <Route exact path="/settings" component={Settings} />
         {/* </Switch> */}
       </Router>
     </div>
