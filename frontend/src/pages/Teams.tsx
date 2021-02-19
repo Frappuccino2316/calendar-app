ceb22298d3a4b8639afaa55a37db1a19ae1d9e4f
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-19 12:36:21 +0900

    Team画面でチーム名を表示

--- frontend/src/pages/Teams.tsx
+++ frontend/src/pages/Teams.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import { apiConfig } from 'commons/apiConfig';
 import Auth from 'components/Auth';
 
 const Teams: React.FC = () => {
-  // const [team, setTeam] = React.useState('');
+  const [teamName, setTeamName] = React.useState('');
+  const [cookie] = useCookies();
+  const baseUrl = apiConfig.apiUrl;
 
-  // React.useEffect(() => {
-
-  // }, [])
+  React.useEffect(() => {
+    axios
+      .get(`${baseUrl}myself/`, {
+        headers: {
+          Authorization: `JWT ${cookie.calendarJWT}`,
+        },
+      })
+      .then((res) => {
+        setTeamName(res.data.team_of_affiliation);
+      });
+  }, [cookie, baseUrl]);
 
   return (
     <Auth>
       <h1>Teams</h1>
       <p>所属チーム</p>
-      {/* <p>{teamName}</p> */}
+      <p>{teamName}</p>
     </Auth>
   );
 };
