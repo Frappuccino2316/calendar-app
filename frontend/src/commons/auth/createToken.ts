4a660b403239b145ba0186742b672910d8d1c83f
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-20 00:00:28 +0900

    コメントアウトを削除

--- frontend/src/commons/auth/createToken.ts
+++ frontend/src/commons/auth/createToken.ts
@@ -12,11 +12,7 @@ const createToken = async (inputUsername: string, inputPassword: string) => {
       password: inputPassword,
     })
     .then((res) => {
-      // if (res.status !== 200) {
-      //   token = null;
-      // } else {
       token = res.data.access;
-      // }
     })
     .catch((e) => {
       token = null;
