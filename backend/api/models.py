bfed88b6cc0b88c87b790f8e340ef4d10e926fe3
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-25 00:36:00 +0900

    カラムを追加

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,8 +10,23 @@ class Team(models.Model):
 
 
 class Task(models.Model):
+    STATUS_CHOICES = (
+        ('planning', '企画中'),
+        ('dev_ready', '開発待ち'),
+        ('developing', '開発中'),
+        ('qa', 'テスト中'),
+        ('done', '終了')
+    )
+    
     title = models.CharField(max_length=50)
     team_in_charge = models.ForeignKey(Team, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='planning'
+    )
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
