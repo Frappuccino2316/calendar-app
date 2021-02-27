338b8de1ea08a4a7bdbe6b20333aa3014beb4e5d
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-27 22:40:51 +0900

    backendのテストを修正

--- backend/api/tests/test_models.py
+++ backend/api/tests/test_models.py
@@ -3,5 +3,7 @@ from api.models import Team
 
 def test_passing():
     model = Team(
+        id=1,
         team_name="test_team",
     )
+    assert model.team_name == "test_team"
--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -1,9 +1,19 @@
 import pytest
 from django.core.exceptions import ValidationError
 from users.models import Users
+from api.models import Team
 
 def test_email_not_null_validation():
-    model = Users(
+    team = Team(
+        team_name="test_team"
+    )
+    user = Users(
         username="test1",
         password="testtest",
+        email="test@example.com",
+        team_of_affiliation=team
     )
+    assert user.username == "test1"
+    assert user.password == "testtest"
+    assert user.email == "test@example.com"
+    assert user.team_of_affiliation == team
