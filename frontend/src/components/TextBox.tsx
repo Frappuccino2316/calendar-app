7ca4264af4fdbeca95ae158cc93e3ec9c1c33ba4
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-27 21:34:51 +0900

    valueをstateとして扱うテキストボックスコンポーネントを作成

---
+++ frontend/src/components/TextBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {
+  value: string;
+  setValueFunction: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const TextBox: React.FC<Props> = ({ value, setValueFunction }) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+        setValueFunction(e.target.value)
+      }
+    />
+  );
+};
+
+export default TextBox;
