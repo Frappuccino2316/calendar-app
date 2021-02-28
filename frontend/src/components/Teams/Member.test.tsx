8a3c0d1cd01ef2d9278fa57ef429df19e15409f3
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-28 21:36:49 +0900

    不要なimportを削除

--- frontend/src/components/Teams/Member.test.tsx
+++ frontend/src/components/Teams/Member.test.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import { apiConfig } from 'commons/apiConfig';
 import Member from 'components/Teams/Member';
 
 afterEach(cleanup);
