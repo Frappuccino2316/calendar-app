4fa3d6f932710f13909c76a80dce4bfd10258a92
Frappuccino001 <fumi00hiro@icloud.com>
2021-02-28 22:05:42 +0900

    MembersListコンポーネントを実装

---
+++ frontend/src/components/Teams/MembersList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Member from 'components/Teams/Member';
+
+type User = {
+  id: number;
+  username: string;
+  email: string;
+  team_of_affiliation: number;
+};
+
+type Props = {
+  membersInformation: User[];
+};
+
+const MembersList: React.FC<Props> = ({ membersInformation }) => {
+  const members = membersInformation.map((member: User) => {
+    return <Member key={member.id} user={member} />;
+  });
+
+  return <ul>{members}</ul>;
+};
+
+export default MembersList;
