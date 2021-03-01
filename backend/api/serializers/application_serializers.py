f1d4eba01bb18dc94759073d10a1dcd2b7903ace
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-01 22:38:13 +0900

    serializerをPOST用と分けて作成

--- backend/api/serializers/application_serializers.py
+++ backend/api/serializers/application_serializers.py
@@ -4,10 +4,18 @@ from api.models.application_models import ApplicationToTeam
 class ApplicationToTeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationToTeam
+        fields = ('id', 'applicant', 'application_team')
+
+class ApplicationToTeamCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicationToTeam
         fields = ('id', 'application_team')
     
     def create(self, validated_data):
         user = self.context['request'].user
+        
+        if user.team_of_affiliation:
+            raise serializers.ValidationError("Already belong to team")
         
         application = ApplicationToTeam(
             applicant = user,
