46ba9214b5c3ff339b5e865a1a8db6ca81de4856
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-01 12:52:22 +0900

    Usersモデルに申請状況を追加

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,6 +4,15 @@ from django.utils.translation import gettext_lazy as _
 from api.models import Team
 
 class Users(AbstractUser):
+    APPLICATION_STATUS = (
+        ('applying', '申請中'),
+        ('unapplied', '未申請'),
+    )
+    
     email = models.EmailField(_('email address'), null=False)
     team_of_affiliation = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
-    
+    application_status_to_team = models.CharField(
+        max_length=9,
+        choices=APPLICATION_STATUS,
+        default='unapplied',
+    )
