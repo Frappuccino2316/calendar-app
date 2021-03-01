310f2f1d75427daf95da7822fbd15e694e2b0796
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-01 20:53:52 +0900

    typo

--- backend/api/models/application_models.py
+++ backend/api/models/application_models.py
@@ -2,5 +2,5 @@ from django.db import models
 from users.models import models
 
 class ApplicationToTeam(models.Model):
-    applicant = ForeignKey(Users, on_delete=models.CASCADE)
-    application_team = ForeignKey(Team, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(Users, on_delete=models.CASCADE)
+    application_team = models.ForeignKey(Team, on_delete=models.CASCADE)
