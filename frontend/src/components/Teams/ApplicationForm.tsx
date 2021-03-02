8c56b6e5cd99efbeea0a435e433e2699e0ee0d17
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-02 23:49:07 +0900

    チームをオプションとして表示

---
+++ frontend/src/components/Teams/ApplicationForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Cookies, useCookies } from 'react-cookie';
+import axios from 'axios';
+import { apiConfig } from 'commons/apiConfig';
+
+const baseUrl: string | undefined = apiConfig.apiUrl;
+
+type Team = {
+  id: string;
+  team_name: string;
+  created_at: string;
+  updated_at: string;
+};
+
+const ApplicationForm = () => {
+  const [teams, setTeams] = React.useState<Team[]>([]);
+  const [cookie] = useCookies();
+
+  React.useEffect(() => {
+    axios
+      .get(`${baseUrl}teams/`, {
+        headers: {
+          Authorization: cookie.calendarJWT,
+        },
+      })
+      .then((response) => {
+        setTeams(response.data);
+      });
+  });
+
+  const teamOptions = teams.map((team) => {
+    <option value={team.team_name}>{team.team_name}</option>;
+  });
+
+  return (
+    <div>
+      <h5>チーム所属申請</h5>
+      <select name="team">{teamOptions}</select>
+      <button>申請</button>
+    </div>
+  );
+};
+
+export default ApplicationForm;
