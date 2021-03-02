44ffa2e547e37fe198fa78e76f75820f77c76a30
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-03 01:00:29 +0900

    ApplicationFormを修正

--- frontend/src/components/Teams/ApplicationForm.tsx
+++ frontend/src/components/Teams/ApplicationForm.tsx
@@ -14,6 +14,7 @@ type Team = {
 
 const ApplicationForm = () => {
   const [teams, setTeams] = React.useState<Team[]>([]);
+  const [isApplication, setIsApplication] = React.useState<boolean>(false);
   const [cookie] = useCookies();
 
   React.useEffect(() => {
@@ -26,17 +27,58 @@ const ApplicationForm = () => {
       .then((response) => {
         setTeams(response.data);
       });
-  }, []);
 
-  const teamOptions = teams.map((team) => {
-    return <option value={team.team_name}>{team.team_name}</option>;
-  });
+    axios
+      .get(`${baseUrl}my_application/`, {
+        headers: {
+          Authorization: `JWT ${cookie.calendarJWT}`,
+        },
+      })
+      .then((response) => {
+        const myselfApplication = response.data === [] ? false : true;
+        if (myselfApplication) {
+          setIsApplication(true);
+        } else {
+          setIsApplication(false);
+        }
+      });
+  }, [cookie]);
+
+  const presenceTeam = teams === [] ? true : false;
+
+  let teamOptions: JSX.Element[] | JSX.Element = [];
+  if (presenceTeam) {
+    teamOptions = teams.map((team) => {
+      return <option value={team.team_name}>{team.team_name}</option>;
+    });
+  } else {
+    teamOptions = (
+      <option value="チームが存在しません">チームが存在しません</option>
+    );
+  }
+
+  let applicationButton: JSX.Element;
+  if (presenceTeam) {
+    applicationButton = <button>申請</button>;
+  } else {
+    applicationButton = <button disabled>申請</button>;
+  }
+
+  const createApplication = async () => {
+    axios
+      .post(`${baseUrl}applicant_create/`, {
+        headers: {
+          Authorization: `JWT ${cookie.calendarJWT}`,
+        },
+      })
+      .then((response) => {});
+  };
 
   return (
     <div>
       <h5>チーム所属申請</h5>
       <select name="team">{teamOptions}</select>
-      <button>申請</button>
+      {applicationButton}
     </div>
   );
 };
