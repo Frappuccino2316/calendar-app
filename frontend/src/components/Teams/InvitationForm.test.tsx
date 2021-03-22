3e03c6f9660faee1aa4fd34ae04740e19cf6af7a
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-22 23:22:44 +0900

    InvitationFormのテストを作成

---
+++ frontend/src/components/Teams/InvitationForm.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import axios from 'axios';
+// import MockAdapter from 'axios-mock-adapter';
+// import { apiConfig } from 'commons/apiConfig';
+import InvitationForm from 'components/Teams/InvitationForm';
+
+// const baseUrl: string | undefined = apiConfig.apiUrl;
+
+// const mock = new MockAdapter(axios);
+
+afterEach(cleanup);
+
+describe('ApplicationForm', () => {
+  it('Should display team name and can click select box', async () => {
+    render(<InvitationForm />);
+    expect(screen.findByText('申請')).toBeTruthy();
+  });
+});
