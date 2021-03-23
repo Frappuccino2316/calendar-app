5b6c687548a1c6df5f1b4ba9bbc7b5589efcb122
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-23 12:45:36 +0900

    create serializer of invite

---
+++ backend/api/serializers/invitation_create_serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from api.models.application_models import ApplicationToTeam
+from users.models import Users
+
+class ApplicationToTeamCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicationToTeam
+        fields = ('id', 'applicant')
+    
+    def create(self, validated_data):
+        team_user = self.context['request'].user
+        invited_user = Users.objects.get(email=validated_data['email'])
+        
+        if invited_user.team_of_affiliation:
+            raise serializers.ValidationError("Already belong to team")
+        
+        application = ApplicationToTeam(
+            applicant = invited_user,
+            application_team = team_user.team_of_affiliation
+        )
+        application.save()
+        return application
