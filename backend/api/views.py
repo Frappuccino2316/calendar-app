721c7a8d11e924059ac4c53f1a5677d38be7999b
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-24 23:45:45 +0900

    delete_not_unused_import

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, mixins, viewsets, generics, status
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
