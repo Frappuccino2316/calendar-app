5ed3436af8ac101ec29f1a0d6cd4ea98f3b969bb
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-24 22:04:27 +0900

    招待を取り消す処理を追加

--- frontend/src/components/Teams/InvitationList.tsx
+++ frontend/src/components/Teams/InvitationList.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import { apiConfig } from 'commons/apiConfig';
 
 type User = {
   id: number;
@@ -18,6 +21,22 @@ type Props = {
 };
 
 const InvitationList: React.FC<Props> = ({ invitations }) => {
+  const baseUrl: string | undefined = apiConfig.apiUrl;
+  const [isDelete, setIsDelete] = React.useState<boolean>(false);
+  const [cookie] = useCookies();
+
+  const deleteInvitation = (id: number) => {
+    axios
+      .delete(`${baseUrl}invitation_delete/${id}/`, {
+        headers: {
+          Authorization: `JWT ${cookie.calendarJWT}`,
+        },
+      })
+      .then(() => {
+        setIsDelete(true);
+      });
+  };
+
   const usersList = invitations.map((invitation) => {
     <tr>
       <td>{invitation.applicant.username}</td>
@@ -29,16 +48,19 @@ const InvitationList: React.FC<Props> = ({ invitations }) => {
   });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ユーザー名</th>
-          <th>メールアドレス</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>{usersList}</tbody>
-    </table>
+    <div>
+      {isDelete && <p>招待を取り消しました</p>}
+      <table>
+        <thead>
+          <tr>
+            <th>ユーザー名</th>
+            <th>メールアドレス</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{usersList}</tbody>
+      </table>
+    </div>
   );
 };
 
