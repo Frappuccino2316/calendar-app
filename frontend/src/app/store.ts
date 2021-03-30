4d71d45bf1e34d6978a5284bb1d75335604177fd
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-30 23:17:49 +0900

    RootStateの型を定義

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from 'slices/user/userSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     user: userReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
