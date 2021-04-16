e423d8a626cad1c6d434e7bfc0306290a7e2dcba
Frappuccino001 <fumi00hiro@icloud.com>
2021-04-16 22:53:32 +0900

    idを追加

--- frontend/src/components/tasks/TaskRow.tsx
+++ frontend/src/components/tasks/TaskRow.tsx
@@ -3,26 +3,20 @@ import React from 'react';
 interface TaskProps {
   id: number;
   title: string;
-  team_in_charge: number;
   status: string;
   start_date: string;
   end_date: string;
-  created_at: string;
-  updated_at: string;
 }
 
 const TaskRow: React.FC<TaskProps> = ({
   id,
   title,
-  team_in_charge,
   status,
   start_date,
   end_date,
-  created_at,
-  updated_at,
 }) => {
   return (
-    <tr>
+    <tr key={id}>
       <td>{title}</td>
       <td>{status}</td>
       <td>{start_date}</td>
