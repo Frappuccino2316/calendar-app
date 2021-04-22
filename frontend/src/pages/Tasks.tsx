ec046d9e44a711d5aec9fafc632c6740a9966df2
Frappuccino001 <fumi00hiro@icloud.com>
2021-04-22 19:49:46 +0900

    Task画面にTaskTableを追加

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Auth from 'components/Auth';
 import Title from 'components/Title';
+import TaskTable from 'components/tasks/TaskTable';
 
 const Tasks: React.FC = () => {
+  const tasks = [
+    {
+      id: 0,
+      title: 'Task1',
+      status: 'planning',
+      start_date: '2021-04-16',
+      end_date: '2021-04-18',
+    },
+    {
+      id: 1,
+      title: 'Task2',
+      status: 'dev_ready',
+      start_date: '2021-04-19',
+      end_date: '2021-05-17',
+    },
+  ];
   return (
     <Auth>
       <Title title="Task" />
+      <TaskTable tasks={tasks} />
     </Auth>
   );
 };
